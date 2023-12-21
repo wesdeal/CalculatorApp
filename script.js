db8a5077a7e666e6d947cbc93b44e2b95f4c0942
Wesley Deal <wesdeall@gmail.com>
2023-12-21 00:20:23 -0500

    realized onlick events for each button was obsolete, implemented event listener to display each digit

--- script.js
+++ script.js
@@ -1,3 +1,49 @@
+let operationCalled = false;
+
+let digit = document.querySelectorAll('.digits');
+digit.forEach(button => {
+    button.addEventListener('click', function() {
+        display(button.value);
+    });
+});
+
+
 function display(num) {
     document.getElementById("calculation-box").value += num;
 }
+
+function clear() {
+    document.getElementById('calculation-box').value = '';
+}
+
+function operation(operator) {
+    operationCalled = true;
+    //switch case
+    switch (operator) {
+        case "+":
+            
+        case "-":
+            break;
+        case "*":
+            break;
+        case "/":
+            break;
+    }
+}
+
+function input(num) {
+    //displays first number typed
+    if (!operationCalled) {
+        display(num)
+    }
+    //once operator is clicked, wait for second num,
+    //once second num is inputed() (use input func again) clear display first and perform calculation
+}
+
+function calculation() {
+
+}
+
+
+//things to fix later on,
+// do not allow user to type more than one decimal point
