642e5a7f17b1f86f0369cb1efc0684d9b3ad9fb1
Wesley Deal <wesdeall@gmail.com>
2023-12-26 01:20:41 -0500

    added click event listeners for digit buttons in main function, worked on input and handler functions

--- script.js
+++ script.js
@@ -1,18 +1,36 @@
 let runningTotal = 0;
+let firstNumber = null;
+let currentNumber = null;
+let currentOperator = null; 
 const screen = document.getElementById('screen');
-let num = document.getElementsByClassName('.digits');
+//let num = document.getElementsByClassName('.digits');
 
+function updateDisplay() {
+    screen.value = currentNumber !== null ? currentNumber : firstNumber !== null ? firstNumber : '0';
+}
 
 function numInput(num) {
     if (isNaN(num)) {
-            
+        handleOperator(num);
+    } else {
+        handleNumber(num);
     }
 }
 
-function handleOperator() {
-    
+function handleNumber(num) {
+    if (currentNumber === null) {
+        currentNumber = num;
+    } else {
+        currentNumber += num;
+    }
+    updateDisplay();
 }
 
+function handleOperator() {
+    if (currentNumber === null) {
+
+    }
+}
 
 function math(operation) {
     if (operation === "+") { // addition
@@ -26,13 +44,29 @@ function math(operation) {
     } else {
         return;
     }
- 
 }
 
+
 function main() {
+    const digits = document.querySelectorAll('.digits');
+    digits.forEach(digit => {
+        digit.addEventListener('click', () => {
+            numInput(parseInt(digit.value));
+        });
+    });
+
+    
     
 }
-    
+
+function numInput(num) {
+    if (isNaN(num)) {
+        handleOperator(num);
+    } else {
+        handleNumber(num);
+    }
+}
+
 main();
 
 
